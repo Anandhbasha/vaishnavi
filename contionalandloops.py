f9dcafddec9b7736ad61c6c0072688373dc11e48
Anandhbasha <anadhakm@gmail.com>
2025-12-03 12:20:40 +0530

    first commit

---
+++ contionalandloops.py
@@ -0,0 +1,20 @@
+# if "condition":
+#     print()
+
+# age = int(input("Enter your age:"))
+# if age>19:
+#     print("He is adult")
+# elif age<13:
+#     print("He is Kid")
+# else:
+#     print("He is not a adult")
+
+# looping
+# while
+# count = 1
+# while count<=3:
+#     print("Max attempt reached")
+#     count += 1
+# for
+for i in range(3):
+    print("Max attempt reached")
---
+++ index.py
@@ -0,0 +1,62 @@
+# a=10
+# # datatypes
+# # simple data types (Primitive)
+# # int
+# a=10
+# # float
+# a=10.0
+# # string
+# name="xyz"
+# # boolean
+# alive =True
+# # complex
+# # list
+# list = [10,20,30]
+# list[2] = 50
+# list.append(60)
+# print(list[2])
+# # tuple
+# tuple = (60,70,80)
+# print(tuple[0])
+# # set
+# sets = {101,101,102,105,104,103}
+# print(sets)
+# # dictionary
+# person = {
+#     "name":"abc",
+#     "age":20
+# }
+
+# print(person["age"])
+
+
+# print("Hello welcome",end='-')
+# print("Python Page")
+
+# print("green","red","blue",sep='_')
+
+# # operators
+# # arithmetic
+# # **,//
+# print(2**3)
+# print(10/2)
+# print(10//2)
+# # logical
+# # and or not
+# print(10<15 and 5<7 and 6>8)
+# print(10<15 and 5<7 or 6>8)
+# print(not 10==5)
+# relationship (comparision)
+# bitwise
+# membership operator
+list = ["apple","orange","banana"]
+print("apple" in list)
+# identity
+list1 = list
+list2 = ["apple","orange","banana"]
+
+
+print(list is list1)
+print(list is list2)
+
+print(type(list2))
